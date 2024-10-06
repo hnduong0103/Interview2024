d7a7646e914d854ecffa8b2da3b1b098692e87ae
Hong Nhat Duong <hnduong0103@gmail.com>
2024-10-06 09:53:00 +0700

    Validate input type with TryParse

--- Interview2024/Program.cs
+++ Interview2024/Program.cs
@@ -8,14 +8,29 @@ namespace Interview2024
     {
         static void Main(string[] args)
         {
+            //Instruction
             Console.WriteLine("Problem: A candle finishes burning would leave a leftover. k leftovers can be combined to make a new candle");
             Console.WriteLine("You first have n candles. How many candles can you burn in total?");
             Console.WriteLine("Example: (n, k) -> (5, 2) = 9");
-            Console.Write("Enter Num of Candles n = ");
-            int numOfCandles = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter Rate of combine k = ");
-            int combineRate = Convert.ToInt32(Console.ReadLine());
 
+            //Get input
+            Console.Write("Enter Num of Candles n = ");
+            int numOfCandles;
+            int combineRate;
+            while (!int.TryParse(Console.ReadLine(), out numOfCandles))
+            {
+                Console.WriteLine("Input must be integer");
+                Console.Write("Retry enter Num of Candles n = ");
+            }
+           
+            Console.Write("Enter Rate of combine k = ");
+            while (!int.TryParse(Console.ReadLine(), out combineRate))
+            {
+                Console.WriteLine("Input must be integer");
+                Console.Write("Retry enter Combine rate n = ");
+            }
+
+            //Validate
             var input = new CandleProblemInput(numOfCandles, combineRate);
             var context = new ValidationContext(input, null, null);
             var validationResults = new List<ValidationResult>();
@@ -29,6 +44,7 @@ namespace Interview2024
             }
             else
             {
+                //Compute and print
                 var solver = new Sol2_Recursion();
                 Console.Write($"Total candles that you can burn: {solver.GetTotalBurnTime(input)}");
             }
